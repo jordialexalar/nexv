8ff51bcaa75fa441884625cf4b1e396288ad7eed
jordialexalar <registres003@gmail.com>
2024-11-24 10:33:34 +0100

    sdf

---
+++ prova.c
@@ -0,0 +1,4 @@
+#include "main.h"
+#include "can_power_member.h"     // Header file (needed)
+//#include "DSP2833x_Device.h"      // Adapt to your needs
+//External function needed: canTransmitStdMsg(&canFrame) -> This function must be implemented by the user and is platform dependent
